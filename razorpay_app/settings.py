b9dce2e89a04390dcadd0637dbba577ef893122e
srb1998 <svgupta@mitaoe.ac.in>
2023-04-07 19:01:28 +0530

    configured static files

--- razorpay_app/settings.py
+++ razorpay_app/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_ROOT = 'templates'
+STATIC_URL = '/src/templates'
 
 django_heroku.settings(locals())
 
