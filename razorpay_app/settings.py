7389172a4035389e4b50c25946e59b017e2410a6
srb1998 <svgupta@mitaoe.ac.in>
2023-04-07 19:55:02 +0530

    configured static files

--- razorpay_app/settings.py
+++ razorpay_app/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'razorpay_app', 'static'),
+    os.path.join(BASE_DIR, 'src', 'static'),
 ]
 
 
